46935fdc074b8538943827569f1e9166e6ee22dd
pandurx <internalfire90@gmail.com>
2019-10-09 22:29:13 -0400

    fixed error

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+//import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify';
+import VTextField from 'vuetify/es5/components/VTextField'
 
 Vue.use(Vuetify);
 
